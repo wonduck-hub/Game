a542bd68bf9c56c0e0705dd8a1f83a0e04ad4c9a
wonduck-hub <60185513+wonduck-hub@users.noreply.github.com>
2023-02-13 00:49:33 +0900

    Human class 조금 만듦

---
+++ Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+	internal class Human
+	{
+		private String name;
+		private int life = 100;
+		private int power = 10;
+		private int defense = 5;
+
+		public Human(string name)
+		{
+			this.name = name;
+		}
+
+		public void attack(Human enemy)
+		{
+			enemy.life -= this.power - enemy.defense;
+		}
+	}
+}
